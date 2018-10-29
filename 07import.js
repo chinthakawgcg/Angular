7f7f84a78617406b8aac38bbab87ac5756b85d35
chinthakawgcg <30571902+chinthakawgcg@users.noreply.github.com>
2018-10-29 15:01:48 +0530

    Create 07import.js

---
+++ 07import.js
@@ -0,0 +1,9 @@
+##we can use classes in othe files
+
+import {Puppy} from './puppy';
+
+var hotdog = new Puppy('Édouard');
+console.log(hotdog.bark());
+// Let's create more puppies
+var oscar = new Puppy('Oscar-Claude');
+console.log(oscar.bark());
