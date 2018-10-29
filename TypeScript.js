09afd49987f979e04d8203275100be392e066717
chinthakawgcg <30571902+chinthakawgcg@users.noreply.github.com>
2018-10-29 12:17:28 +0530

    Create TypeScript.js

---
+++ TypeScript.js
@@ -0,0 +1,19 @@
+# Angular
+For learning
+
+// define array as Array<Type>
+const cats: Array<string> = ['Simba', 'Aslan'];
+// Type[] does the same thing.
+const cats2: string[] = ['Simba', 'Aslan'];
+
+interface Cat {
+  name: string,
+  age: number
+}
+
+const betterCats: Array<Cat> = [
+  {name: 'Simba', age: 22},
+  {name: 'Aslan', age: 9999}
+];
+
+console.log(betterCats);
